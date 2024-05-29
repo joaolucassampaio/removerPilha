516c899cccb8566623406e5324a9eecf7476bcee
João Lucas Sampaio <160886470+joaolucassampaio@users.noreply.github.com>
2024-05-29 14:07:44 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,62 +2,88 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct node {
-	int info;
-	struct node *next;
+struct node{
+  int info;
+  struct node *next;
 };
 
-int main(int argc, char *argv[]) {
-	struct node *pilha, *p, *pilhaAux, *temp, *atual;
-	int count = 0;
-	pilha = NULL;
-	
-	srand(time(NULL));
-	
-	printf("Pilha original:\n");
-	do {
-		p = malloc(sizeof(struct node));
-		p->info = rand()%100;
-		p->next = pilha;
-		pilha = p;
-		count++;
-		printf("%d ", p->info);
-	}while(count < 5);
-	
-	pilhaAux = pilha;
-	
-	printf("\n%d", pilhaAux->info);
-	
-	count = 0;
-	//Tá removendo o nó da frente do nó selecionado aqui
-	while(count < 1) {
-	    pilhaAux = pilhaAux->next;
-	    count++;
-	}
-	printf("\n%d", pilhaAux->info);
-	
-	//O problema deve estar aqui
-	//Não quero quero que remova o nó da frente do selecionado, mas sim o nó selecionado acima
-	temp = pilhaAux->next;
-	pilhaAux->next = pilhaAux->next->next;
-    free(temp);
-    
-	printf("\nPilha apos a remocao:\n");
-	struct node *prev = NULL;
-	while (pilha != NULL) {
-	    struct node *nextNode = pilha->next;
-	    pilha->next = prev;
-	    prev = pilha;
-	    pilha = nextNode;
-	}
-	
-	atual = prev;
-	while(atual != NULL) {
-	    printf("%d ", atual->info);
-	    atual = atual->next;    
+int main (int argc, char *argv[]){
+  struct node *topo, *p, *q, *r, *z, *w, *pilhaAux;
+  int count = 0;
+  topo = NULL;
+  pilhaAux = NULL;
+
+  srand (time (NULL));
+
+  //Empilhando
+  while (count < 5){
+	  p = malloc (sizeof (struct node));
+	  p->info = rand () % 100;
+	  p->next = topo;
+	  topo = p;
+	  count++;
 	}
 
-	
-	return 0;
-	
+  //Imprimindo Pilha:
+  r = topo;
+  printf ("Pilha original:\n");
+  while (r != NULL){
+	  printf ("%d\n", r->info);
+	  r = r->next;
+	}
+
+  //Encontrando terceiro no
+  count = 0;
+  q = topo;
+  printf ("\nTerceiro no: ");
+  while (count < 2){
+	  q = q->next;
+	  count++;
+	}
+  printf ("%d\n", q->info);
+
+  //Desempilhando
+  count = 0;
+  while (count <= 2){
+	  z = topo;
+	  topo = topo->next;
+
+	  if (count == 2)
+		{
+		  free (z);
+		}
+	  else
+		{
+		  z->next = pilhaAux;
+		  pilhaAux = z;
+		}
+	  count++;
+	}
+    
+    //Se eu printar isso aqui a pilha nÃ£o funciona
+    w = pilhaAux;
+    // Imprimindo os nos colocados na pilha auxiliar
+    printf ("\nNos na pilha auxiliar:\n");
+    while (w != NULL){
+	  printf ("%d\n", w->info);
+	  w = w->next;
+	}
+    
+    //Empilhando de volta
+    while (pilhaAux != NULL){
+        z = pilhaAux;
+        pilhaAux = pilhaAux->next;
+        z->next = topo; // Atualiza o prÃ³ximo nÃ³ do elemento desempilhado para apontar para o topo da pilha principal
+        topo = z; // Atualiza o topo da pilha principal para apontar para o elemento desempilhado
+    }
+
+  //Imprimindo pilha apos empilhar de volta
+  printf ("\nPilha apos empilhar de volta:\n");
+  z = topo;
+  while (z != NULL){
+	  printf ("%d\n", z->info);
+	  z = z->next;
+	}
+
+  return 0;
 }
